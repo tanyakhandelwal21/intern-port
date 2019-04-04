7ae62e86d0cf305e78fb9a402d7b2df6cd12ffef
aditya8100 <deepak2@purdue.edu>
2019-04-04 19:36:51 -0400

    Email and Password Sign In Implemented

--- server.js
+++ server.js
@@ -15,19 +15,72 @@ require('firebase/auth')
 require('firebase/database')
 
 app.listen(process.env.PORT || 1337, () => console.log("Server is listening"))
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/authentication", (req, res) => {
-    let body = req.body;
+    var user_details = req.body.user_details;
+    
+    console.log("Encoded: " + user_details)
+    user_details = Buffer.from(user_details, 'base64');
+    console.log("Decoded: " + user_details)
 
-    let email = body.email;
-    let password = body.password;
+    user_details = '' + user_details
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
-        if (error != null) {
-            console.log(error.message)
-            return
-        }
+    let emailID = user_details.substring(0, user_details.indexOf(":"));
+    let password = user_details.substring(user_details.indexOf(":")+1);
 
-        console.log("User signed in")
-    })
+    console.log("Email: " + emailID)
+
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(emailID)) {
+        res.send({"Status": "Error", "Message": "Invalid Email ID."})
+        return
+    }
+    
+    if (password.length < 6) {
+        res.send({"Status": "Error", "Message": "Password must be atleast 6 characters."})
+        return
+    }
+
+    if (req.body.action_type != null && req.body.action_type == "Create") {
+        firebase.auth().createUserWithEmailAndPassword(emailID, password).then((user) => {
+            console.log("User created")
+            if (user && user.emailVerified === false) {
+                user.sendEmailVerification().then(function(){
+                    console.log("email verification sent to user");
+                });
+            }
+            res.send({"Status": "Success"})
+        }).catch((error) => {
+            if (error != null) {
+                console.log(error.message)
+                res.send({"Status": "Error", "Message": error.message})
+            }
+        })
+    } else if (req.body.action_type != null && req.body.action_type == "Sign In") {
+        firebase.auth().signInWithEmailAndPassword(emailID, password).then((user) => {
+            if (user && user.emailVerified === false) {     
+                res.send({"Status": "Error", "Message": "Unverified Email Address"})
+            } else {
+                console.log("User signed in")
+                res.send({"Status": "Success", "UID": firebase.auth().currentUser.uid})
+            }
+        }).catch((error) => {
+            if (error != null) {
+                console.log(error.message)
+                res.send({"Status": "Error", "Message": error.message})
+            }
+        })
+
+    } else if (req.body.action_type != null && req.body.action_type == "Sign Out") {
+        firebase.auth().signOut().then(() => {          
+            console.log("User signed out")
+            res.send({"Status": "Success"})
+        }).catch((error) => {
+            if (error != null) {
+                console.log(error.message)
+                res.send({"Status": "Error", "Message": error.message})
+            }
+        })
+
+    } 
 })
