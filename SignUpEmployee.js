1a430a293ebc7bc0f0fb07326fee6192784e2ada
aditya8100 <deepak2@purdue.edu>
2019-04-06 00:52:19 -0400

    Create Account Backend

---
+++ SignUpEmployee.js
@@ -0,0 +1,28 @@
+function create_account() {
+    console.log("Inside function!")
+    let password = document.getElementById("pass").value;
+    let confirmed_password = document.getElementById("cpass").value
+
+    if (password != confirmed_password) {
+        alert("Passwords don't match. Try again.")
+        return
+    }
+
+    let emailID = document.getElementById("email").value
+
+    var combination = emailID + ":" + password
+    combination = btoa(combination)
+
+    let data_json = {
+        "user_details": combination,
+        "action_type": "Create",
+        "auth_type": "Employee"
+    }
+
+    $.post("https://cors-anywhere.herokuapp.com/https://intern-port-server.herokuapp.com/authentication", data_json, (data, status) => {
+
+        if (data.Status == "Error") {
+            alert(data.Message)
+        }
+    })
+}
