78e65d0eee071be1957668008d2c7e1f06657051
aditya8100 <deepak2@purdue.edu>
2019-04-18 15:04:58 -0400

    Add members duplicates prevented

--- AddMembers.js
+++ AddMembers.js
@@ -1,19 +1,12 @@
 $(document).ready(() => {
+    let name = []
+    console.log(JSON.stringify(name))
+    localStorage.setItem("members", JSON.stringify(name))
     let group_name = localStorage.getItem("group")
     $("#title").text("Add Members to " + group_name)
 
     $("#submit").click(() => {
         let company_name = localStorage.getItem("company")
-        let email_id = $("#email").val()
-        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!re.test(email_id)) {
-            alert("Enter a valid email ID")
-            return
-        }
-        if (email_id == "") {
-            alert("You have to enter an email ID")
-            return
-        }
 
         let data_json = {
             "email": email_id,
@@ -25,5 +18,29 @@ $(document).ready(() => {
             console.log(data)
             window.location.href = "CompanyDashboard.html"
         })
+    })
+
+    $("#add_member").click(() => {
+        let email_id = $("#email").val()
+        let members = JSON.parse(localStorage.getItem("members"))
+        $("#email").val("")
+        if (email_id == "") {
+            alert("You have to enter an email ID")
+            return
+        }
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email_id)) {
+            alert("Enter a valid email ID")
+            return
+        }
+        console.log(members)
+        console.log(members.indexOf(email_id))
+
+        if (members.indexOf(email_id) == -1) {
+            $("select").prepend("<option value=\"" + email_id + "\">" + email_id + "</option>")
+            members[members.length] = email_id
+            localStorage.setItem("members", JSON.stringify(members))
+        }
+        console.log(members)
     })
 })
